064b7d14d489cf82c7e87b2b3aa7539947e236b1
Haard Panchal <17704777+h44rd@users.noreply.github.com>
2018-12-29 01:30:45 +0300

    Removed privacy sensitive information

--- Constants.java
+++ Constants.java
@@ -1,6 +1,6 @@
 package com.cvit.haard.attendance;
 
 public class Constants {
-    public static final String UPLOAD_URL = "http://cvit.iiit.ac.in/biometrics/simple.php";
-//    public static final String IMAGES_URL = "http://192.168.94.1/AndroidImageUpload/getImages.php";
+    public static final String UPLOAD_URL = "*removed for privacy reasons*";
+//    public static final String IMAGES_URL = "*removed for privacy reasons*";
 }
